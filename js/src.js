d6ac2e5b827b40ab717779f2337005a2f26f77ef
Alex Davidson <alexdavidson4@gmail.com>
2023-04-01 15:24:08 -0400

    Added the rest of the functions for checking hands

--- js/src.js
+++ js/src.js
@@ -19,7 +19,7 @@ function getSortedRanks(hand){
     handnums = [];
     for(i = 0; i < hand.length; i++){
         handnums[i] = hand[i][RANK_POS];
-        handnums[i] = lowAceCardOrderMap[handnums[i]]
+        handnums[i] = highAceCardOrderMap[handnums[i]]
     }
     handnums.sort(sortNum);
     return handnums;
@@ -54,7 +54,13 @@ function check_high_ace_straight(hand) {
 }
 
 function check_low_ace_straight(hand) {
-    handnums = getSortedRanks(hand)
+    handnums = [];
+    for(i = 0; i < hand.length; i++){
+        handnums[i] = hand[i][RANK_POS];
+        handnums[i] = lowAceCardOrderMap[handnums[i]]
+    }
+    handnums.sort(sortNum);
+    return handnums;
 
     for(i = 1; i < handnums.length; i++){
         if(handnums[i-1] +1 != handnums[i]){
@@ -69,7 +75,7 @@ function check_straight(hand){
 }
 
 function check_quads(hand){
-    handnums = getSortedRanks(hand)
+    handnums = getSortedRanks(hand);
     for(i = 0; i < 2; i++){
         if(handnums[i] == handnums[i+1] && handnums[i] == handnums[i+2] && handnums[i] == handnums[i+3]){
             return true;
@@ -78,39 +84,57 @@ function check_quads(hand){
     return false
 }
 
-//Testing Flush function
-let hand = ["5H", "8H", "AH", "2H", "TH"]
-let hand2 = ["5H", "8H", "AS", "2H", "TH"]
+function check_full_house(hand){
+    handnums = getSortedRanks(hand);
+    if(handnums[0] == handnums[1] && handnums[0] == handnums[2] && handnums[3] == handnums[4]){
+        return true;
+    }
+    if(handnums[0] == handnums[1] && handnums[2] == handnums[3] && handnums[2] == handnums[4]){
+        return true;
+    }
+    return false;
+}
 
-console.log(check_flush(hand));
-console.log(check_flush(hand2));
+function check_trips(hand){
+    handnums = getSortedRanks(hand);
+    for(i = 0; i < 3; i++){
+        if(handnums[i] == handnums[i+1] && handnums[i] == handnums[i+2]){
+            return true;
+        }
+    }
+    return false
+}
 
-//Testing Straight function
-hand1 = ["5C", "7H", "6H", "8H", "4D"]
-hand2 = ["5C", "8H", "AH", "2H", "TD"]
-hand3 = ["5C", "4H", "AH", "2H", "3D"]
+function check_two_pair(hand){
+    handnums = getSortedRanks(hand);
+    console.log(handnums);
+    for(i = 0; i < 4; i++){
+        if(handnums[i] == handnums[i+1]){
+            handnums.splice(i, 2);
+            console.log(handnums);
+            for(j = 0; j < 2; j++){
+                if (handnums[j] == handnums[j+1]){
+                    return true
+                }
+            }
+            return false;
+        }
+    }
+    return false
+}
 
-console.log(check_straight(hand1));
-console.log(check_straight(hand2));
-console.log(check_straight(hand3));
+function check_high_pair(hand){
+    handnums = getSortedRanks(hand);
+    for(i = 0; i < 4; i++){
+        if(handnums[i] == handnums[i+1] && handnums[i] > 10){
+            return true;
+        }
+    }
+    return false
+}
 
-//Testing Straight Flush function
-hand1 = ["5H", "7H", "6H", "8H", "4H"]
-hand2 = ["5D", "7H", "6H", "8H", "4D"]
-hand3 = ["5H", "JH", "6H", "8H", "4H"]
-
-console.log(check_straight_flush(hand1))
-console.log(check_straight_flush(hand2))
-console.log(check_straight_flush(hand3))
-
-//Testing Quads function
-hand1 = ["5H", "5D", "5C", "5S", "TH"]
-hand2 = ["5D", "QC", "TH", "8S", "AD"]
-
-console.log(check_quads(hand1))
-console.log(check_quads(hand2))
-
-
+hand1 = ["TH", "7D", "6H", "AC", "6S"]
+console.log(check_high_pair(hand1))
 
 
 
